8aef329212bdab3ee13c80a8f47e53c2083f146d
Joshua Liebow-Feeser <joshlf13@gmail.com>
2012-08-15 20:53:09 -0400

    Debugging commit - temporary

--- postpone.go
+++ postpone.go
@@ -89,7 +89,7 @@ func (p *postpone) Read(buf []byte) (int, error) {
 	}
 	i, err := p.rs.Read(buf)
 	err = errlist.NewError(err).AddError(p.err).Err()
-	fmt.Printf("Error: %s\n",err)
+	fmt.Println(err)
 	return i, errlist.NewError(err).AddError(p.err).Err()
 }
 
